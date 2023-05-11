7aa5a3f61802228f512a8a158083ad5f0f717ac5
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-05-11 18:02:26 +0900

    ✨ ログイン機能実装

---
+++ db/schema.rb
@@ -0,0 +1,8 @@
+create_table "users", force: :cascade do |t|
+  t.string "provider"
+  t.string "uid"
+  t.string "name"
+  t.string "email"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
